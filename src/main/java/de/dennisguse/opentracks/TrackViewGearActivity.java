d69f4de0039af9723f168c46d8b00ebe37ace4a5
Sean Gregory <chiefsgreg2622@gmail.com>
2024-04-27 17:42:22 -0400

    Found out how to populate dropdown

--- src/main/java/de/dennisguse/opentracks/TrackViewGearActivity.java
+++ src/main/java/de/dennisguse/opentracks/TrackViewGearActivity.java
@@ -2,6 +2,8 @@ package de.dennisguse.opentracks;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 
@@ -20,6 +22,16 @@ public class TrackViewGearActivity extends AbstractActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Ski Gear Tracker");
+
+        //get the spinner from the xml.
+        Spinner skiDropdown = findViewById(R.id.skiDropDown);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"coolSki", "epicSki", "ski3"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        skiDropdown.setAdapter(adapter);
 
         setSupportActionBar(viewBinding.bottomAppBarLayout.bottomAppBar);
     }
